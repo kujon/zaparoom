c77f8e874a520d0938be9417b7fedd40462d3243
kujon <jakub.korzeniowski@gmail.com>
2015-09-19 20:26:38 +0100

    Setup CI

---
+++ Readme.md
@@ -0,0 +1,2 @@
+![CircleCI](https://img.shields.io/circleci/project/kujon/zaparoom.svg?style=flat-square)
+![David](https://img.shields.io/david/kujon/zaparoom.svg?style=flat-square)
--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,13 @@ var jshint  = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 
 // Lints all javascript files using JSHint.
-+gulp.task('lint', function () {
-    return gulp.src(['web/**/*.js', 'worker/**/*.js', '!**/bootstrap_es6.js'])
+gulp.task('lint', function () {
+    return gulp
+        .src(['web/**/*.js', 'worker/**/*.js', '!**/bootstrap_es6.js'])
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('default', function () {
+    return;
 });
