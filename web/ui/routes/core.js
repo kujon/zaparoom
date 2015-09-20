3b9dfaa2af9d5c14b2a3c31ab162f0e105c9f840
kujon <jakub.korzeniowski@gmail.com>
2015-09-20 02:21:03 +0100

    Ensured create element follows react's api

--- web/ui/routes/core.js
+++ web/ui/routes/core.js
@@ -4,7 +4,7 @@ import App from '../components/app';
 export default function setup(app) {
     app.get('/', (req, res) => {
         // Render the app to string.
-        let content = renderToString(App, {});
+        let content = renderToString(App, {}, null, null);
         // Render the response.
         res.render('index.ejs', {title: 'zaparoom', content: content});
     });
--- web/utils/react.js
+++ web/utils/react.js
@@ -2,13 +2,13 @@ import React from 'react/addons';
 import { curry, compose, useWith, call, identity } from 'ramda';
 import { IO } from 'ramda-fantasy';
 
-// createElement :: ReactClass -> Object -> ReactElement
-var createElement = useWith(call, React.createFactory, identity);
+// createElement :: ReactClass -> Object -> * -> ReactElement
+var createElement = useWith(call, React.createFactory, identity, identity);
 
 // render :: ReactElement -> DOM -> IO ReactElement
 var render = curry((element, target) => new IO(() => React.render(element, target)));
 
-// renderToString :: ReactClass -> Object -> String
+// renderToString :: ReactClass -> Object -> * -> String
 var renderToString = compose(React.renderToString, createElement);
 
 export { createElement, render, renderToString };
