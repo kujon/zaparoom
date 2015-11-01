cbafcef0cfdaa3a01038f26c04608df4cbf4a66d
kujon <jakub.korzeniowski@gmail.com>
2015-11-01 19:47:33 +0000

    Extending users table

--- db/deploy/users.sql
+++ db/deploy/users.sql
@@ -6,10 +6,14 @@ BEGIN;
 SET client_min_messages = 'warning';
 
 CREATE TABLE zaparoom.users (
-    first_name      TEXT    NOT NULL,
-    last_name       TEXT    NOT NULL,
-    type            TEXT,
-    date_of_birth   DATE
+    first_name          TEXT    NOT NULL,
+    last_name           TEXT    NOT NULL,
+    type                TEXT,
+    date_of_birth       DATE,
+    sex                 INT,
+    employment_status   INT,
+    profession          TEXT,
+    country_of_birth    TEXT
 );
 
 COMMIT;
--- db/verify/users.sql
+++ db/verify/users.sql
@@ -2,7 +2,14 @@
 
 BEGIN;
 
-SELECT first_name, last_name, type, date_of_birth
+SELECT first_name,
+        last_name,
+        type,
+        date_of_birth,
+        sex,
+        employment_status,
+        profession,
+        country_of_birth
     FROM zaparoom.users
     WHERE FALSE;
 
